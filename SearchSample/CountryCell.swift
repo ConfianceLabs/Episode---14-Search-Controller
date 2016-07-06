a4bd0eaec7442b5a05e28c68ec633daa207e5272
ConfianceLabs <confiancelabs@gmail.com>
2016-07-07 00:21:17 +0530

    search Controller

---
+++ SearchSample/CountryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountryCell.swift
+//  SearchSample
+//
+//  Created by apple on 24/04/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+import UIKit
+
+class CountryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
